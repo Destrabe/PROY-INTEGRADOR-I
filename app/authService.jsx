f51c7289913dcd2c450ac0206e0d8680abe5b022
Destrabe20 <marco96392@gmail.com>
2026-05-01 10:14:45 -0500

    implement user registration and login functionality

---
+++ app/authService.jsx
@@ -0,0 +1,38 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "@/firebase/db";
+import { auth } from "@/firebase/auth";
+
+export const loginUser = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const registerUser = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    const uid = userCredential.user.uid;
+    await setDoc(doc(db, "users", uid), {
+      email,
+      userId: uid,
+    });
+    return { success: true };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,5 +1,44 @@
+"use client";
+import { useState } from "react";
+import { registerUser } from "../authService";
+
 function RegisterPage() {
-  return;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    console.log("Inventando registrar con: ", email, password);
+    const result = await registerUser(email, password);
+    if (!result.success) {
+      if (result.error.code === "auth/email-already-in-use") {
+        return alert("el correo ya esta registrado");
+      }
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-card">
+        <form className="register-form" onSubmit={handleRegister}>
+          <h2>Registro</h2>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Contraseña"
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;
